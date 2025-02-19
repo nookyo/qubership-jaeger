0c51ef3807b85ee58b4c8c513d2024c8bcc596b4
Vadim Naumov <redthowdy@gmail.com>
2025-02-19 18:34:55 +0600

    fix linter

--- integration-tests/robot/tests/libs/JaegerLibrary.py
+++ integration-tests/robot/tests/libs/JaegerLibrary.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 
 
 def generate_trace():
-    with open('./tests/libs/resources/spans.json') as f:
+    with open("./tests/libs/resources/spans.json") as f:
         data = json.load(f)
 
     timestamp = _generate_timestamp()
@@ -12,11 +12,11 @@ def generate_trace():
     parent_id = _generate_id(16)
 
     fs = data[0]
-    fs['traceId'] = parent_id
-    fs['id'] = parent_id
-    fs['timestamp'] = timestamp
-    fs['annotations'][0]['timestamp'] = timestamp
-    fs['annotations'][1]['timestamp'] = timestamp + 14861
+    fs["traceId"] = parent_id
+    fs["id"] = parent_id
+    fs["timestamp"] = timestamp
+    fs["annotations"][0]["timestamp"] = timestamp
+    fs["annotations"][1]["timestamp"] = timestamp + 14861
     return data
 
 
@@ -26,4 +26,4 @@ def _generate_timestamp():
 
 
 def _generate_id(n):
-    return ''.join(random.choices('0123456789abcdef', k=n))
+    return "".join(random.choices("0123456789abcdef", k=n))
